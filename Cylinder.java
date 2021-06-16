80126a9816c83dc6ffff73f2b4412bd7d6267d5b
rijul244 <78681948+rijul244@users.noreply.github.com>
2021-06-16 18:28:58 +0400

    Add files via upload
    Cylinder java file added

---
+++ Cylinder.java
@@ -0,0 +1,35 @@
+package LAB1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Cylinder {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Please enter radius and height:");
+		try {
+			System.out.print("Radius= ");
+			float Radius=input.nextFloat();
+			System.out.print("Height= ");
+			float Height=input.nextFloat();
+			System.out.println("The cylinder's area is "+Area(Radius,Height)+"cm2");
+			System.out.println("The cylinder's volume is "+Volume(Radius,Height)+"cm2");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Invalid Input (Only numbers allowed)");
+		}
+		input.close();
+	}
+
+	public static int Area(float r, float h)
+	{
+		return (int) Math.round((2*Math.PI*r*(r+h)));		
+	}
+	public static int Volume(float r, float h)
+	{
+		return (int) Math.round((Math.PI*Math.pow(r, 2)*h));		
+	}
+	
+
+}
